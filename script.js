ddcd594a6a6d0a1938e93dfb906a1433b10cd0d5
0bobcode <bobchopracoding@gmail.com>
2024-12-10 11:50:52 +0530

    add 2 functionalites load question and checkanwser and there is a bug that the texxt wont display in the outputy labely

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+// const questions = [
+//     {
+//         question: "Q1.what is full form of https",
+//         options: ["hyper text transfer protocall secure", "hyper text translation protocal secured", "highest text translator perfected secure", "hyper text transfer language unsecure"],
+//         correctAnswer: 1
+        
+//     },
+//     {
+//         question: "Q2. define array",
+//         options: ["it is an varible","programming language","a data structure that stores elements of the same type","a website that allows you to get fonts"],
+//         correctAnswer: 3
+//     }
+// ];
+// console.log(questions.question);
+
+// let currentQuestionIndex = 0
+
+// function loadQuestion(){
+//     let currentQuestion = questions[currentQuestionIndex]
+//     document.getElementById("question").textContent = questions.question
+//     const optBtn = document.querySelector(".answer-btn")
+//     optBtn.forEach((button , index)=>{
+//         button.textContent = questions.options[index]
+//         button.disabled = false
+//     });
+
+//     document.getElementById("feedback").textContent = "";
+//     document.getElementById("next-btn").style.display = "none";
+// }
+
+
+// loadQuestion()
